dc96a49ba8fef24a63f913285e9a45c2ec4dbf9f
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-07-20 13:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function rot13(encodedStr) {
 	return decodedArr.join('');
   // Only change code below this line
 
-  return; //return decodedArr
+   //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
