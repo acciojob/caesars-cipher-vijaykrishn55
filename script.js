1555b6f54981d033b19f7459bb23ba265a68ed51
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-07-20 13:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
 	for (let i=0;i<encodedStr.length;i++){
-		if (lookup[encodedStr[i]===undefined]){
+		if (lookup[encodedStr[i]==undefined]){
 		decodedArr.push(encodedStr[i])
 	}else{
 		decodedArr.push(lookup[encodedStr[i]])
